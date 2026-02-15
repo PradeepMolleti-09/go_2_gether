b667e301fc9927437225b63451e7aebe6bd7f07c
PradeepMolleti-09 <pradeepmolleti09@gmail.com>
2026-02-15 17:24:12 +0530

    URL issue Resolved

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,7 +6,7 @@ export const env = {
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT) || 4000,
   mongoUri: process.env.MONGO_URI || "mongodb+srv://pradeepmolleti09_db_user:Vi9Ex9DjuD6Xr1iE@cluster0.7ir9i67.mongodb.net/?appName=Cluster0",
-  clientOrigin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+  clientOrigin: process.env.CLIENT_ORIGIN || "https://go2gether-iota.vercel.app",
   jwtSecret: process.env.JWT_SECRET || "MY_SECRET_KEY",
   googleClientId: process.env.GOOGLE_CLIENT_ID || "1018923804236-dvh398b37aecmh2t2k2f9i9tuuhpdbvo.apps.googleusercontent.com",
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET || "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",
